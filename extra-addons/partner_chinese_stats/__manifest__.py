e2f5d28ffe557b8ca8f5c48493172fa04935cd9d
jorge <jdurancruz@danielcastelao.org>
2025-12-05 08:26:16 +0100

    Modelo zodiaco listo

---
+++ extra-addons/partner_chinese_stats/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "partner_chinese_stats",
+
+    'summary': "Módulo que permite ver el signo del horóscopo chino",
+
+    'description': """
+Este módulo extiende el modelo res_partner añadiendo 3 campos
+    """,
+
+    'author': "Jorge Durán Cruz",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','contacs'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
---
+++ extra-addons/partner_chinese_stats/models/models.py
@@ -0,0 +1,56 @@
+import datetime  #-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class partner_chinese_stats(models.Model):
+     _inherit = 'res.partner'
+
+
+     f_nac = fields.Date("Fecha de nacimiento")
+     edad = fields.Integer(string = "Edad", readonly = True, compute = "_calcular_edad", store = True)
+     signo_chino = fields.Char(string = "Signo Chino",readonly = True, compute="_calcular_chinada", store=True)
+
+     @api.depends('f_nac')
+     def _calcular_edad(self):
+         for record in self:
+             if record.f_nac:
+                 fecha_actual = datetime.datetime.now()
+                 tiempo_vivido = fecha_actual - record.f_nac
+                 record.edad = tiempo_vivido.year
+                 
+     @api.depends('f_nac')
+     def _calcular_chinada(self):
+         for record in self:
+             if record.f_nac:
+                 ano = record.f_nac.year
+                 signoNum = ano%12
+                 signo = "Sin signo"
+
+                 match(signoNum):
+                     case 0:
+                         signo = "Mono"
+                     case 1:
+                         signo = "Gallo"
+                     case 2:
+                         signo = "Perro"
+                     case 3:
+                         signo = "Cerdo"
+                     case 4:
+                         signo = "Rata"
+                     case 5:
+                         signo = "Buey"
+                     case 6:
+                         signo = "Tigre"
+                     case 7:
+                         signo = "Conejo"
+                     case 8:
+                         signo = "Dragón"
+                     case 9:
+                         signo = "Serpiente"
+                     case 10:
+                         signo = "Caballo"
+                     case 11:
+                         signo = "Cabra"
+
+                 record.signo_chino = signo
